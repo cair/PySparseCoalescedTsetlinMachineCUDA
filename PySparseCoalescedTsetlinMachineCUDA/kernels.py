2dcddaca337d2380b496701db9b2e5f28c1a7968
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-17 13:43:44 +0200

    Update

--- PySparseCoalescedTsetlinMachineCUDA/kernels.py
+++ PySparseCoalescedTsetlinMachineCUDA/kernels.py
@@ -267,7 +267,13 @@ code_evaluate = """
     extern "C"
     {
         // Evaluate examples
-        __global__ void evaluate(unsigned int *global_ta_state, int *clause_weights, int number_of_nodes, int *class_sum, int *X, int example)
+        __global__ void evaluate(
+            unsigned int *global_ta_state,
+            int *clause_weights,
+            int number_of_nodes,
+            int *class_sum,
+            int *X,
+            int example)
         {
             int index = blockIdx.x * blockDim.x + threadIdx.x;
             int stride = blockDim.x * gridDim.x;
