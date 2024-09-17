77632f0ac0adeff1cd16c380d276f64d381da861
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-09-17 15:01:18 +0200

    Update

--- examples/SimpleGraphDemoTest.py
+++ examples/SimpleGraphDemoTest.py
@@ -109,17 +109,4 @@ for i in range(tm.number_of_clauses):
                     l.append("NOT x%d" % (k - args.hypervector_size))
         print(" AND ".join(l))
 
-
-start_training = time()
-tm.fit(graphs_train, Y_train, epochs=1, incremental=True)
-stop_training = time()
-
-start_testing = time()
-result_test = 100*(tm.predict(graphs_test) == Y_test).mean()
-stop_testing = time()
-
-result_train = 100*(tm.predict(graphs_train) == Y_train).mean()
-
-print("%.2f %.2f %.2f %.2f" % (result_train, result_test, stop_training-start_training, stop_testing-start_testing))
-
 print(graphs_train.hypervectors)
