1a5fdf86f6d0bf1df03fecf1eb15ebc6012ed55c
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-09-05 16:02:55 +0200

    Update

--- PySparseCoalescedTsetlinMachineCUDA/tm.py
+++ PySparseCoalescedTsetlinMachineCUDA/tm.py
@@ -206,8 +206,6 @@ class CommonTsetlinMachine():
 #define NUMBER_OF_EXAMPLES %d
 """ % (self.number_of_outputs, self.number_of_clauses, self.number_of_features, self.number_of_state_bits, self.boost_true_positive_feedback, self.s, self.T, self.q, self.number_of_clause_groups, self.max_included_literals, self.negative_clauses, self.number_of_patches, X.shape[0])
 
-		print(parameters)
-
 		mod_encode = SourceModule(parameters + kernels.code_header + kernels.code_encode, no_extern_c=True)
 		self.encode = mod_encode.get_function("encode")
 		self.encode.prepare("PPPiiiiii")
